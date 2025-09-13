58423c35dca564f0b8793c516e1b621838362b33
Егор Деревягин <egord1010@mail.ru>
2025-09-13 20:07:11 +0700

    feat/(IS-1): init

---
+++ src/main/kotlin/com/informationsecuritylab1/InformationSecurityLab1Application.kt
@@ -0,0 +1,11 @@
+package com.informationsecuritylab1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InformationSecurityLab1Application
+
+fun main(args: Array<String>) {
+    runApplication<InformationSecurityLab1Application>(*args)
+}
---
+++ src/test/kotlin/com/informationsecuritylab1/InformationSecurityLab1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.informationsecuritylab1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InformationSecurityLab1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
