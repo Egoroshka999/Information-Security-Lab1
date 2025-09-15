543df9976f21048211fa6c185a83064cf921da27
Егор Деревягин <egord1010@mail.ru>
2025-09-16 03:05:53 +0700

    feat/(IS-1): fix

--- src/main/kotlin/com/informationsecuritylab1/config/JwtFilter.kt
+++ src/main/kotlin/com/informationsecuritylab1/config/JwtFilter.kt
@@ -5,11 +5,9 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.userdetails.User
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import java.util.*
 
 @Component
 class JwtFilter(private val jwtUtil: JwtUtil, private val userDetailsService: CustomUserDetailsService) : OncePerRequestFilter() {
