bb3d98c3b6db1abff727f928d3f56bb252fa1db6
Егор Деревягин <egord1010@mail.ru>
2025-09-16 04:08:37 +0700

    feat/(IS-1): fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.4") // только для компиляции
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.4")
+    spotbugs("com.github.spotbugs:spotbugs-annotations:4.9.4")
 }
 
 kotlin {
